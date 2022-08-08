a3e69a7815d8287a3c7d142d4e79b09f16766b0d
Tsedal Lombebo <Brightlink01@gmail.com>
2022-08-08 18:40:51 +0000

    commiting the file end

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Module to test the function rectangle"""
+
+import unittest
+import rectangle
+
+class TestRectangle(unittest.TestCase):
+
+    def test_rectangle_output(self):
+        
+        self.assertAlmostEqual(rectangle(,)
+
+    def test_rectangle_input(self):
+        
+        self.assertRaises(TypeError, rectangle, )
