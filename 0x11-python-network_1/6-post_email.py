b58a2837eed3adb7177ab5a1269328b43985797d
Tsedal Lombebo <Brightlink01@gmail.com>
2022-10-23 12:10:53 +0000

    quick

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-""" takes URL and Email as arguments. sends a POST request to the
-    passed URL with the email as parameter. and finally displays the
-    body of the response.
 """
-
-import requests
+Python script that takes in a URL and
+an email address, sends a POST request
+"""
 from sys import argv
+import requests
+
 
 if __name__ == "__main__":
-    url = argv[1]
-    email = {'email': argv[2]}
-
-    r = requests.post(url, data=email)
-    print(r.text)`
+    r = requests.post(argv[1], data={'email': argv[2]})
+    print(r.text)
